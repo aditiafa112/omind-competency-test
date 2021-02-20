494f341b298e56165b654fb58f17cfd5289aee4a
aditiafa <aditiafa112@gmail.com>
2021-02-20 14:22:23 +0700

    change name app

--- src/assets/illustrations/index.js
+++ src/assets/illustrations/index.js
@@ -1,5 +1,6 @@
 import ILHome from './tab-home.svg';
 import ILLive from './tab-live.svg';
 import ILProfile from './tab-profile.svg';
+import ICSplash from './splash.svg';
 
-export {ILHome, ILLive, ILProfile};
+export {ILHome, ILLive, ILProfile, ICSplash};
--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,14 +1,28 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {StyleSheet, View, Animated} from 'react-native';
 import {useDispatch} from 'react-redux';
 import {SetLogin} from '../../redux/actions';
 import {colors, getData} from '../../utils';
+import {ICSplash} from '../../assets';
 
 const Splash = ({navigation}) => {
   const dispatch = useDispatch();
+  const translateY = useRef(new Animated.Value(80)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
-    setTimeout(() => {
+    Animated.timing(translateY, {
+      useNativeDriver: true,
+      toValue: 0,
+      duration: 1000,
+    }).start();
+
+    Animated.timing(opacity, {
+      useNativeDriver: true,
+      toValue: 1,
+      duration: 1000,
+      delay: 500,
+    }).start(() => {
       getData('login').then((res) => {
         if (res) {
           dispatch(SetLogin(res));
@@ -17,12 +31,20 @@ const Splash = ({navigation}) => {
           navigation.replace('Login');
         }
       });
-    }, 1000);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <View style={styles.page}>
-      <Text>Welcome</Text>
+      <Animated.View
+        style={[
+          styles.logoWrapper,
+          {
+            transform: [{translateY}],
+          },
+        ]}>
+        <ICSplash />
+      </Animated.View>
     </View>
   );
 };
@@ -35,5 +57,14 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background.primary,
     padding: 20,
     justifyContent: 'center',
+  },
+  logoWrapper: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
